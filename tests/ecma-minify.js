21a40e8f7340b9ae3df9eb48fccb3c4102793e0c
polygonplanet <polygon.planet.aqua@gmail.com>
2015-10-05 20:00:33 +0900

    Add test source

---
+++ tests/ecma-minify.js
@@ -0,0 +1,14 @@
+var regex=/[\/\\"']*/g;
+var str='string';
+var multilineString="aaa\"bbb'\
+ccc";
+var array=[
+0x0FFF,1e8,0,12345,
+3.524603890386048e+24,
+0.0006215
+];
+var i=0;
+var もじ='/*"もじ"*/';
+var a=1&&2||3;
+var i=0;
+var n=(i++ +i);
