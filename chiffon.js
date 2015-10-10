90ec971df14f39681e120f8cd49baad38c2bdd96
polygonplanet <polygon.planet.aqua@gmail.com>
2015-10-10 18:10:42 +0900

    Improve tokenize pattern and minifying.

--- chiffon.js
+++ chiffon.js
@@ -4,7 +4,7 @@
  * @description  A very small ECMAScript parser, tokenizer and minify written in JavaScript
  * @fileoverview JavaScript parser, tokenizer and minify library
  * @version      1.1.4
- * @date         2015-10-06
+ * @date         2015-10-10
  * @link         https://github.com/polygonplanet/Chiffon
  * @copyright    Copyright (c) 2015 polygon planet <polygon.planet.aqua@gmail.com>
  * @license      Licensed under the MIT license.
@@ -13,7 +13,7 @@
 /*jshint bitwise:false, eqnull:true */
 (function(name, context, factory) {
 
-  // Supports UMD. AMD, CommonJS/Node.js and browser context
+  // Supports AMD, Node.js, CommonJS and browser context.
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
       module.exports = factory();
@@ -31,27 +31,25 @@
 
   var Chiffon = {};
 
-  var hasOwnProperty = Object.prototype.hasOwnProperty;
+  var DEFAULT_MAXLINELEN = 32000;
 
-  var Keywords = (function(words) {
-    words = words.split(/\s+/);
-    var kw = {};
-    for (var i = 0, len = words.length; i < len; i++) {
-      kw[words[i]] = 1;
-    }
-    return kw;
+  var isKeyword = (function(words) {
+    var keywords = new RegExp('^(?:' + words.split(/\s+/).join('|') + ')$');
+    return function(ident) {
+      return keywords.test(ident);
+    };
   }(
     // ECMA-262 11.6.2.1 Keywords
-    'break do in typeof case else instanceof var ' +
-    'catch export new void class extends return while const finally ' +
-    'super with continue for switch yield debugger function this default ' +
-    'if throw delete import try ' +
-    'arguments eval ' +
+    'var function this return if else typeof for const new break do ' +
+    'in void case instanceof catch export class extends while finally ' +
+    'super with continue switch yield debugger default ' +
+    'throw delete import try ' +
+    // Reserved keywords
+    'let static ' +
     // ECMA-262 11.6.2.2 Future Reserved Words
     'enum await ' +
     'implements package protected interface private public'
   ));
-
 
   var _Comment = 'Comment',
       _LineTerminator = 'LineTerminator',
@@ -66,29 +64,30 @@
       _Boolean = 'Boolean',
       _Keyword = 'Keyword';
 
+  var capturedToken = [
+    '',
+    _Comment,
+    _Comment,
+    _LineTerminator,
+    _Comment,
+    _Template,
+    _String,
+    _RegularExpression,
+    _Numeric,
+    _Punctuator,
+    _UnicodeEscapeSequence,
+    _LineTerminator,
+    _Identifier
+  ];
+  var capturedTokenLen = capturedToken.length;
+  var regularExpressionIndex = 7;
 
-  var TokenName = {
-    2: _Comment,
-    3: _Comment,
-    4: _LineTerminator,
-    5: _Comment,
-    6: _Template,
-    7: _String,
-    8: _Punctuator,
-    9: _LineTerminator,
-    10: _RegularExpression,
-    11: _Numeric,
-    12: _Punctuator,
-    13: _UnicodeEscapeSequence,
-    14: _LineTerminator,
-    15: _Identifier
-  };
-
+  var identRe = /[^\s+\/%*=&|^~<>!?:;,.()[\]{}'"`-]/;
+  var signRe = /[+-]/;
 
   var lineTerminator = '\\r\\n\\u2028\\u2029';
   var lineTerminatorSequence = '(?:\\r\\n|[' + lineTerminator + '])';
   var whiteSpace = '(?:(?![' + lineTerminator + '])\\s)+';
-  var regexpFlags = makeRegExpFlagPattern('gimuy');
   var literalSuffix = '(?=' +
     '\\s*' +
     '(?:' + '(?!\\s*[/\\\\<>*+%`^"\'\\w$-])' +
@@ -101,43 +100,50 @@
       '|' + '$' +
     ')' +
   ')';
+  var punctuators = '(?:' +
+          '>>>=?|[.]{3}|<<=|===|!==|>>=' +
+    '|' + '[+][+](?=[+])|--(?=-)' +
+    '|' + '[=!<>*%+/&|^-]=' +
+    '|' + '&&|[|][|]|[+][+]|--|<<|>>|=>' +
+    '|' + '[-+*/%<>=&|^~!?:;,.()[\\]{}]' +
+  ')';
+  var regexpLiteral = '(?:' +
+    '/(?![*])(?:\\\\.|[^/' + lineTerminator + '\\\\])+/' +
+    '(?:[gimuy]{0,5}|\\b)' +
+  ')';
+  var validRegExpPrefix = new RegExp('(?:' +
+          '(?:^(?:typeof|in|void|case|instanceof|yield|throw|delete)$)' +
+    '|' + '(?:' + '(?![.)\\]])' + punctuators + '$)' +
+  ')');
 
-  var tokenizePattern = new RegExp(
-    '(' + // (2) multiline comment
-          '(/[*][\\s\\S]*?[*]/)' +
-          // (3) single line comment
+  var tokenizePatternAll = new RegExp(getPattern(true), 'g');
+  var tokenizePattern = new RegExp(getPattern(), 'g');
+
+  function getPattern(all) {
+    return '(?:' +
+          // (1) multiline comment
+          '(' + '/[*][\\s\\S]*?[*]/' + ')' +
+          // (2) single line comment
     '|' + '(' + '//[^' + lineTerminator + ']*' +
           '|' + '<!--[^' + lineTerminator + ']*' +
           ')' +
-          // (4) line terminators
+          // (3) line terminators
     '|' + '(?:^|(' + lineTerminatorSequence + '))' +
           '(?:' + whiteSpace + '|)' +
-          // (5) single line comment
+          // (4) single line comment
           '(' + '-->[^' + lineTerminator + ']*' +
           ')' +
-          // (6) template literal
+          // (5) template literal
     '|' + '(' + '`(?:\\\\[\\s\\S]|[^`\\\\])*`' +
           ')' + literalSuffix +
-          // (7) string literal
+          // (6) string literal
     '|' + '(' + '"(?:\\\\[\\s\\S]|[^"' + lineTerminator + '\\\\])*"' +
           '|' + "'(?:\\\\[\\s\\S]|[^'" + lineTerminator + "\\\\])*'" +
           ')' +
-          // (8) regexp literal prefix
-    '|' + '(' + '^' +
-          '|' + '[-!%&*+,/:;<=>?[{(^|~]' +
-          ')' +
-          '(?:' + whiteSpace +
-          // (9) line terminators
-          '|' + '(' + lineTerminatorSequence + ')' +
-          ')?' +
-          '(?:' +
-            // (10) regular expression literal
-            '(' +
-                '(?:/(?![*])(?:\\\\.|[^/' + lineTerminator + '\\\\])+/)' +
-                '(?:' + regexpFlags + '|)' +
-            ')' + literalSuffix +
-          ')' +
-          // (11) numeric literal
+          // (7) regular expression literal
+    '|' + '(' + (all ? regexpLiteral : whiteSpace) +
+          ')' + literalSuffix +
+          // (8) numeric literal
     '|' + '(' + '0(?:' + '[xX][0-9a-fA-F]+' +
                    '|' + '[oO][0-7]+' +
                    '|' + '[bB][01]+' +
@@ -146,102 +152,118 @@
           '|' + '[1-9]\\d*' +
           '|' + '0[0-7]+' +
           ')' +
-          // (12) operators
-    '|' + '(' + '>>>=?|[.]{3}|<<=|===|!==|>>=' +
-          '|' + '[+][+](?=[+])|--(?=-)' +
-          '|' + '[=!<>*%+/&|^-]=' +
-          '|' + '&&|[|][|]|[+][+]|--|<<|>>|=>' +
-          '|' + '[-+*/%<>=&|^~!?:;,.()[\\]{}]' +
+          // (9) operators
+    '|' + '(' + punctuators +
           ')' +
-          // (13) unicode character
+          // (10) unicode character
     '|' + '(' + '\\\\u[0-9a-fA-F]{4}' +
           ')' +
     '|' + whiteSpace +
-          // (14) line terminators
+          // (11) line terminators
     '|' + '(' + lineTerminatorSequence + ')' +
-          // (15) identifier
+          // (12) identifier
     '|' + '(' + '[^\\s+/%*=&|^~<>!?:;,.()[\\]{}\'"`-]+' +
           ')' +
-    ')',
-    'g'
-  );
-
-
-  function makeRegExpFlagPattern(flags) {
-    flags = flags.split('');
-
-    var len = flags.length;
-    var patterns = {};
-    var i, j, k, l, m;
-    var c, c2, c3, c4, c5;
-
-    var add = function(f) {
-      if (!/(.).*\1/.test(f)) {
-        patterns[f] = 1;
-      }
-    };
-
-    for (i = 0; i < len; i++) {
-      c = flags[i];
-      patterns[c] = 1;
-
-      for (j = 0; j < len; j++) {
-        c2 = flags[j];
-        add(c + c2);
-
-        for (k = 0; k < len; k++) {
-          c3 = flags[k];
-          add(c + c2 + c3);
-
-          for (l = 0; l < len; l++) {
-            c4 = flags[l];
-            add(c + c2 + c3 + c4);
-
-            for (m = 0; m < len; m++) {
-              c5 = flags[m];
-              add(c + c2 + c3 + c4 + c5);
-            }
-          }
-        }
-      }
-    }
-
-    return '(?:' + getKeys(patterns).join('|') + ')';
+    ')';
   }
 
 
-  function parseMatches(match, options) {
-    var tokens = [];
-    var tokenKeys = getKeys(TokenName);
-
-    for (var i = 0, len = tokenKeys.length; i < len; i++) {
-      var key = tokenKeys[i];
-      var value = match[key];
+  function parseMatches(match, tokens, options) {
+    for (var i = 1; i < capturedTokenLen; i++) {
+      var value = match[i];
       if (!value) {
         continue;
       }
 
-      var name = TokenName[key];
-      if ((name === _Comment && !options.comment) ||
-          (name === _LineTerminator && !options.lineTerminator)) {
-        continue;
-      }
-
-      var type = name;
-      if (name === _Identifier) {
+      var regex;
+      var type = capturedToken[i];
+      if (type === _Comment) {
+        if (!options.comment) {
+          break;
+        }
+      } else if (type === _LineTerminator) {
+        if (!options.lineTerminator) {
+          continue;
+        }
+      } else if (type === _Identifier) {
         if (value === 'null') {
           type = _Null;
         } else if (value === 'true' || value === 'false') {
           type = _Boolean;
-        } else if (hasOwnProperty.call(Keywords, value)) {
+        } else if (isKeyword(value)) {
           type = _Keyword;
         }
+      } else if (type === _RegularExpression) {
+        regex = parseRegExpFlags(value);
       }
 
-      tokens[tokens.length] = {
+      var token = {
         type: type,
         value: value
       };
+
+      if (regex) {
+        token.regex = regex;
+      }
+
+      tokens[tokens.length] = token;
+
+      if (value === match[0]) {
+        break;
+      }
+    }
+  }
+
+
+  // Fix Regular Expression missing matches e.g. `var g=1,a=2/3/g;`
+  function fixRegExpTokens(tokens, options) {
+    for (var i = 0; i < tokens.length; i++) {
+      if (tokens[i].type !== _RegularExpression) {
+        continue;
+      }
+
+      var index = i;
+      var regexToken = tokens[i];
+
+      while (--index >= 0) {
+        var token = tokens[index];
+        var type = token.type;
+
+        if (type === _Comment || type === _LineTerminator) {
+          continue;
+        }
+
+        if ((type === _Punctuator || type === _Keyword) &&
+            validRegExpPrefix.test(token.value)) {
+          break;
+        }
+
+        var parts = parseRegExp(regexToken.value, options);
+        Array.prototype.splice.apply(tokens, [i, 1].concat(parts));
+        break;
+      }
+    }
+  }
+
+
+  function parseRegExpFlags(value) {
+    var index = value.lastIndexOf('/');
+    var flags = value.substring(index + 1);
+    var pattern = value.substr(1, index - 1);
+
+    return {
+      pattern: pattern,
+      flags: flags
+    };
+  }
+
+
+  function parseRegExp(value, options) {
+    var tokens = [];
+    var m;
+
+    while ((m = tokenizePattern.exec(value)) != null) {
+      parseMatches(m, tokens, options);
     }
 
     return tokens;
@@ -251,19 +273,17 @@
   function tokenize(code, options) {
     options = options || {};
 
-    var results = [];
+    var tokens = [];
     var m;
 
-    tokenizePattern.lastIndex = 0;
-    while ((m = tokenizePattern.exec(code)) != null) {
-      var tokens = parseMatches(m, options);
-      for (var i = 0, len = tokens.length; i < len; i++) {
-        results[results.length] = tokens[i];
-      }
+    while ((m = tokenizePatternAll.exec(code)) != null) {
+      parseMatches(m, tokens, options);
     }
+    fixRegExpTokens(tokens, options);
 
-    return results;
+    return tokens;
   }
+
 
   /**
    * Tokenize a string code.
@@ -279,15 +299,17 @@
   Chiffon.tokenize = tokenize;
 
 
-  function untokenize(tokens) {
-    var ident = /[^\s+\/%*=&|^~<>!?:;,.()[\]{}'"`-]/;
-    var sign = /[+-]/;
+  function untokenize(tokens, options) {
+    options = options || {};
 
     var results = [];
+    var maxLineLen = options.maxLineLen || DEFAULT_MAXLINELEN;
+    var lineLen = 0;
     var prev;
 
     for (var i = 0, len = tokens.length; i < len; prev = tokens[i++]) {
       var token = tokens[i];
+      var next = tokens[i + 1];
 
       if (!prev) {
         if (token.type !== _LineTerminator) {
@@ -296,17 +318,31 @@
         continue;
       }
 
-      if (prev.type === _LineTerminator && token.type === _LineTerminator) {
-        continue;
+      var ws = '';
+      if (token.type === _LineTerminator) {
+        if (prev.type === _LineTerminator) {
+          continue;
+        }
+        if (prev.type === _Punctuator || (next && next.type === _Punctuator)) {
+          token.value = '';
+        } else {
+          lineLen = 0;
+        }
+      } else {
+        if ((signRe.test(token.value.slice(-1)) && signRe.test(prev.value.charAt(0))) ||
+            (identRe.test(prev.value.slice(-1)) && identRe.test(token.value.charAt(0)))) {
+          ws = ' ';
+        }
+
+        if (token.type === _Punctuator && lineLen > maxLineLen) {
+          token.value += '\n';
+          lineLen = 0;
+        }
       }
 
-      var space = '';
-      if ((sign.test(token.value) && sign.test(prev.value)) ||
-          (ident.test(prev.value.slice(-1)) && ident.test(token.value.charAt(0)))) {
-        space = ' ';
-      }
-
-      results[results.length] = space + token.value;
+      var value = ws + token.value;
+      lineLen += value.length;
+      results[results.length] = value;
     }
 
     return results.join('');
@@ -321,33 +357,20 @@
   Chiffon.untokenize = untokenize;
 
 
-  function minify(code) {
-    return untokenize(tokenize(code, { lineTerminator: true }));
+  function minify(code, options) {
+    return untokenize(tokenize(code, { lineTerminator: true }), options);
   }
 
   /**
    * Minify JavaScript code.
    *
    * @param {string} code Target code.
+   * @param {Object} [options] minify options.
+   *   - maxLineLen: {number} (default=32000)
+   *     Limit the line length in symbols.
    * @return {string} Return a minified code.
    */
   Chiffon.minify = minify;
-
-
-  function getKeys(object) {
-    if (Object.keys) {
-      return Object.keys(object);
-    }
-
-    var keys = [];
-    for (var key in object) {
-      if (hasOwnProperty.call(object, key)) {
-        keys[keys.length] = key;
-      }
-    }
-    return keys;
-  }
-
 
   return Chiffon;
 }));
