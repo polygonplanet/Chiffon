a3b31b520bec6cb9aaa800d7b8322fc067f5488d
polygonplanet <polygon.planet.aqua@gmail.com>
2015-10-15 10:22:02 +0900

    1.3.0

--- chiffon.js
+++ chiffon.js
@@ -3,8 +3,8 @@
  *
  * @description  A very small ECMAScript parser, tokenizer and minify written in JavaScript
  * @fileoverview JavaScript parser, tokenizer and minify library
- * @version      1.2.1
- * @date         2015-10-14
+ * @version      1.3.0
+ * @date         2015-10-15
  * @link         https://github.com/polygonplanet/Chiffon
  * @copyright    Copyright (c) 2015 polygon planet <polygon.planet.aqua@gmail.com>
  * @license      Licensed under the MIT license.
