58fbb2c2bdfeb63f7834074bdf99faefaf4eabc0
polygonplanet <polygon.planet.aqua@gmail.com>
2026-06-02 17:29:02 +0900

    test: replace duplicate test

--- tests/fixtures/test-0089.js
+++ tests/fixtures/test-0089.js
@@ -1,1 +1,8 @@
-var a;
+var regex = /[//][\/\\"']*/g;
+var regex2 = /*//*//[/]/.test(1);
+var regex3 = /1//*/*/.test(1);
+/*
+var regex4 = /a[/]b/g;
+/*/
+var regex5 = /^/+/* comment *//1//**/.test(1);
+//*/
