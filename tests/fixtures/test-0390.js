15f4c15cdf5ff0b713de45e98da1f4ed04265e01
polygonplanet <polygon.planet.aqua@gmail.com>
2026-07-04 01:16:26 +0900

    test: add tests for Hashbang comments `#!`

---
+++ tests/fixtures/test-0390.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node --eval="/* dummy comment */" // `dummy` 'quote' "str" /re/ -- a**b
+'use strict';
+const dummyValue = '#!test';
+`#!dummy text${dummyValue}`;/#!/.test('213');
---
+++ tests/fixtures/test-0391.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env -S node # shebang #comment with #hashes
+//#!dummy comment
+/*#!dummy block comment*/
+export {};
